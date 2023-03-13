097f056f0d65f78b955883532fc424bbbb230123
Matt <61751438+Mattz-CE@users.noreply.github.com>
2023-03-13 16:05:24 -0700

    AAAA

--- run.py
+++ run.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+
+from forms import RegistrationForm, LoginForm
+from datetime import datetime
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+Bcrypt = Bcrypt(app)
 
 db = SQLAlchemy(app)
+
+# Models
 
 
 class Todo(db.Model):
@@ -19,16 +26,31 @@ class Todo(db.Model):
         return '<Task %r>' % self.id
 
 
-class UserAuth(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(200), nullable=False)
     password = db.Column(db.String(200), nullable=False)
     email = db.Column(db.String(200), nullable=False)
     date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    posts = db.relationship('Post', backref='author', lazy=True)
 
     def __repr__(self):
         return '<User %r>' % self.id
 
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
+# Routes
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -77,6 +99,33 @@ def update(id):
             return 'There was an issue updating your task'
     else:
         return render_template('upda\watchte.html', task=task)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html', title='Register')
+
+    form = RegistrationForm()
+    # if post request
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            hashed_password = bcrypt.generate_password_hash(
+                form.password.data).decode('utf-8')
+            user = User(username=form.username.data,
+                        email=form.email.data, password=hashed_password)
+            db.session.add(user)
+            db.session.commit()
+            flash(f'Account created for {form.username.data}!', 'success')
+            return redirect(url_for('login'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html', title='Login')
 
 
 if __name__ == '__main__':
